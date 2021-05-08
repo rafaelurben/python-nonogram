0674d7498933460d13068d3c6d049144dfdcf4bb
rafaelurben <rafael.urben@gmail.com>
2021-05-08 21:10:53 +0200

    Restructured

--- nonogram/solver/__init__.py
+++ nonogram/solver/__init__.py
@@ -1,1 +1,34 @@
-from nonogram.solver.solver import solve
+"Solver for Nonogram games"
+
+from rich import print
+from rich.rule import Rule
+
+from nonogram.solver.lines import NonogramLine
+
+
+def solve(game):
+    "Solve the board"
+
+    oldhash = hash(str(game.rows))
+
+    # Step 1: individual lines
+    columns = game.cols
+    yinfo = game.yinfo
+    for i in range(game.width):
+        print(Rule(f"Solve column #{i}"))
+        game.replacecol(
+            i, NonogramLine.solve(columns[i], yinfo[i]))
+
+    rows = game.rows
+    xinfo = game.xinfo
+    for i in range(game.height):
+        print(Rule(f"Solve row #{i}"))
+        game.replacerow(
+            i, NonogramLine.solve(rows[i], xinfo[i]))
+
+    newhash = hash(str(game.rows))
+
+    if oldhash != newhash:
+        game.print()
+        print(Rule(title="Next solving cycle"))
+        solve(game)
